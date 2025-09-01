1da39553b65b79a8c3afae5a3996fdd6fe664b86
Bishnuprasad <168080959+Bishnu9078@users.noreply.github.com>
2025-09-01 12:49:15 +0530

    Add files via upload

---
+++ Division_8.java
@@ -0,0 +1,12 @@
+public class Division_8{
+    public static void main(String [] args){
+
+        int a=10;
+        int b= 2;
+
+        int c= a/b;
+        
+        System.out.println("The division between " +a+ "and" +b+ "is:" +c);
+
+    }
+}
