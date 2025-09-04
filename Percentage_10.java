eef89f9b76dc1221c02346083d14f8ebcd5e9f8b
Bishnuprasad <168080959+Bishnu9078@users.noreply.github.com>
2025-09-04 17:54:02 +0530

    Add files via upload

---
+++ Percentage_10.java
@@ -0,0 +1,9 @@
+public class Percentage_10 {
+    public static void main(String [] args){
+        int a= 30;
+        int b= 35;
+
+        int c= (a*100)/b;
+        System.out.println("The percentage is:" + c);
+    }
+}
