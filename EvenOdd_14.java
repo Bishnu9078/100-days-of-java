ceb619139cb776da2306691c88ae5e34a56e75ee
Bishnuprasad <168080959+Bishnu9078@users.noreply.github.com>
2025-09-07 21:37:23 +0530

    Add files via upload

---
+++ EvenOdd_14.java
@@ -0,0 +1,13 @@
+public class EvenOdd_14{
+
+    public static void main(String [] args){
+
+        int a=31;
+        if(a==2){
+            System.out.println("The number is even");
+
+        }else{
+            System.out.println("The number is odd");
+        }
+    }
+}
---
+++ IfElse_12.java
@@ -0,0 +1,12 @@
+public class IfElse_12 {
+    public static void main(String [] args){
+        int a= 30;
+        int b= 35;
+        if(a>b){
+            System.out.println("A is greater than B");
+        }else{
+            System.out.println("B is greater than A");
+        }
+    }
+    
+}
